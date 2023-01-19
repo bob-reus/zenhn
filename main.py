df8998e714d5a698f341600b6fac7d31d390cdf0
Bob Reus <ik@bobre.us>
2023-01-19 21:23:56 +0800

    Add index template and rendering

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from sanic import Sanic
 from sanic.response import text
 
+from models.issue import Issue
+
+issue = Issue()
+
 app = Sanic("zenHN")
+app.extend(config={"oas": False})
+app.static("/static/css", "./static/css", name="css")
+app.static("/static/images", "./static/images", name="images")
 
 
 @app.get("/ping")
@@ -10,8 +17,14 @@ async def ping(request):
 
 
 @app.get("/")
+@app.ext.template("index.html")
 async def index(request):
-    return text("index")
+    return {"app": app, "stories": issue.stories}
+
+
+@app.get("/feed")
+async def feed(request):
+    return text("feed")
 
 
 if __name__ == "__main__":
